77636556f2b5b267a9d557969f410ded903e52a8
yyfyifan <yyfyifan@gmail.com>
2024-01-04 22:28:26 -0800

    Create the project and add dependencies for langchain4j

---
+++ src/main/java/dev/yangyifan/pdfassistant/PdfAssistantApplication.java
@@ -0,0 +1,13 @@
+package dev.yangyifan.pdfassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfAssistantApplication.class, args);
+    }
+
+}
